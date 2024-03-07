2c8c862d3df0a478a38a1c0f6cbc35d875f1f5b2
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-07 15:23:33 +0100

    add

---
+++ script.js
@@ -0,0 +1,52 @@
+const zucchine = [
+  {
+    type: zucchinaUno,
+    peso: 200,
+    lunghezza: 33
+  },
+  {
+    type: zucchinaDue,
+    peso: 150,
+    lunghezza: 30
+  },
+  {
+    type: zucchinaTre,
+    peso: 180,
+    lunghezza: 27
+  },
+  {
+    type: zucchinaQuattro,
+    peso: 300,
+    lunghezza: 20
+  },
+  {
+    type: zucchinaCinque,
+    peso: 190,
+    lunghezza: 26
+  },
+  {
+    type: zucchinaSei,
+    peso: 100,
+    lunghezza: 12
+  },
+  {
+    type: zucchinaSette,
+    peso: 400,
+    lunghezza: 19
+  },
+  {
+    type: zucchinaOtto,
+    peso: 230,
+    lunghezza: 14
+  },
+  {
+    type: zucchinaNove,
+    peso: 290,
+    lunghezza: 40
+  },
+  {
+    type: zucchinaDieci,
+    peso: 50,
+    lunghezza: 7
+  },
+]
